f59a9801885578aec43e4035e0077f7274b57c1d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-27 13:03:29 +0900

    feat: 색상 설정 추가

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,38 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig } from '@pandacss/dev';
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
 
   // Files to exclude
   exclude: [],
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          primary: { value: '#05d686' },
+          clovagreen: { value: '#06CC80' },
+          clovapink: { value: '#FF5C76' },
+          clovablue: { value: '#3894FF' },
+          gray: {
+            100: { value: '#F7F7F7' },
+            200: { value: '#CCCCCC' },
+            300: { value: '#BEBEBE' },
+            400: { value: '#999999' },
+            500: { value: '#666666' },
+            600: { value: '#333333' },
+            700: { value: '#000000' },
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
-  outdir: "styled-system",
+  outdir: 'styled-system',
 });

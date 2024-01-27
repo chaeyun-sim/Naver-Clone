42160176852dc0d8b698713859318d272aa29c32
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-01-27 20:29:12 +0900

    feat: 레이아웃 구현

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,27 @@
+import { Outlet } from 'react-router-dom';
+import { css } from '../../../styled-system/css';
+
+const Layout = () => {
+  return (
+    <div className={container}>
+      <div className={content}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
+
+const container = css({
+  maxWidth: '134rem',
+  width: '100vh',
+  height: '100vh',
+  margin: '0 auto',
+});
+
+const content = css({
+  minHeight: '100vh',
+  flexGrow: 1,
+  margin: '0 3rem',
+});

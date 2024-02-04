e8e0ace2198e8fef368004df969727d08cdbc115
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-04 23:53:26 +0900

    feat: 소셜카드 UI 구현

---
+++ src/components/SocialSction/SocialCard.tsx
@@ -0,0 +1,81 @@
+import { css } from '../../../styled-system/css';
+
+interface Props {
+  text: string;
+  coverUrl: string;
+  profileImage: string;
+  editorName: string;
+}
+
+const SocialCard = ({ text, coverUrl, profileImage, editorName }: Props) => {
+  const handleTextSlice = () => {
+    return text.length > 31 ? text.slice(0, 31) + '...' : text;
+  };
+
+  return (
+    <div className={container}>
+      <img src={coverUrl} className={image} />
+      <p className={textStyle}>
+        <strong>{handleTextSlice()}</strong>
+      </p>
+      <div className={editorBox}>
+        <img src={profileImage} className={profileImageStyle} />
+        <span className={editorStyle}>{editorName}</span>
+      </div>
+    </div>
+  );
+};
+
+export default SocialCard;
+
+const container = css({
+  width: '25%',
+  height: '19.4rem',
+  paddingBottom: '2rem',
+  paddingLeft: '2rem',
+  fontSize: '1.4rem',
+  fontFamily: 'var(--font-nanumsquare-regular)',
+  fontWeight: '500',
+  letterSpacing: '-0.04rem',
+  color: '#101010',
+  lineHeight: '1.7rem',
+});
+
+const image = css({
+  width: '100%',
+  height: '12rem',
+  borderRadius: '0.4rem',
+});
+
+const textStyle = css({
+  marginTop: '1rem',
+  overflow: 'hidden',
+  lineHeight: '2rem',
+  fontSize: '1.4rem',
+  letterSpacing: '-0.04rem',
+  color: '#101010',
+  fontFamily: 'var(--font-nanumsquare-regular)',
+  fontWeight: '500',
+});
+
+const editorBox = css({
+  display: 'flex',
+  marginTop: '0.4rem',
+  lineHeight: '2rem',
+  gap: '0.6rem',
+});
+
+const profileImageStyle = css({
+  width: '2rem',
+  height: '2rem',
+  borderRadius: '2rem',
+  verticalAlign: 'top',
+});
+
+const editorStyle = css({
+  fontFamily: 'var(--font-nanumsquare-regular)',
+  fontWeight: '600',
+  fontSize: '1.3rem',
+  letterSpacing: '-0.04rem',
+  color: '#606060',
+});

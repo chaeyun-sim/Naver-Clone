f06f877b6806ada3b73a1f36cdb81c0e6ae9c1b5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-05 01:41:34 +0900

    feat: 쇼핑몰 그룹 박스 UI 구현

---
+++ src/components/ShoppingSection/MallNameGroup.tsx
@@ -0,0 +1,68 @@
+import { css } from '../../../styled-system/css';
+
+const MallNameGroup = () => {
+  const malls = [
+    ['쿠팡', 'G마켓', '옥션', '11번가'],
+    ['SSG닷컴', '올리브영', 'CJ온스타일', '위메프'],
+    ['GS SHOP', '하프클럽', '패션플러스'],
+  ];
+
+  return (
+    <div className={conatiner}>
+      {malls.map((data) => {
+        return (
+          <div className={box}>
+            {data.map((item, index) => (
+              <>
+                <div className={row}>
+                  {['11번가', 'SSG닷컴', 'GS SHOP'].includes(item) ? (
+                    <strong className={name}>{item}</strong>
+                  ) : (
+                    <span className={name}>{item}</span>
+                  )}
+                </div>
+                {index < data.length - 1 && <div className={dot} />}
+              </>
+            ))}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MallNameGroup;
+
+const conatiner = css({
+  width: '24.8rem',
+  height: '8.8rem',
+  padding: '0.9rem 1.1rem 0.4rem',
+  border: '0.1rem solid rgba(0,0,0,0.05)',
+  backgroundColor: 'rgba(245,248,251,0.6)',
+});
+
+const row = css({
+  paddingBottom: '0.5rem',
+  display: 'flex',
+  flexDirection: 'row',
+});
+
+const dot = css({
+  margin: '0rem 0 0.6rem',
+  width: '0.3rem',
+  height: '0.3rem',
+  backgroundColor: '#D3D5D7',
+  borderRadius: '50%',
+});
+
+const box = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+});
+
+const name = css({
+  fontSize: '1.3rem',
+  letterSpacing: '-0.06rem',
+});

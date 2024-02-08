6823bf8e36b5f91162371c61ff417930823bac8c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-09 00:52:59 +0900

    feat: 굵은 밑줄 분리

---
+++ src/styles/underline.ts
@@ -0,0 +1,8 @@
+import { css } from '../../styled-system/css';
+
+export const boldUnderline = css({
+  '&:hover': {
+    textDecoration: 'underline',
+    textUnderlineOffset: '0.25rem',
+  },
+});

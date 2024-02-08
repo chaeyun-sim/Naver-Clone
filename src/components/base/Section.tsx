6c36c6c0d4e28f38655ef6af70fba970a33d8d54
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-09 00:30:36 +0900

    fix: 커스텀 스타일 추가 및 border 수정

--- src/components/base/Section.tsx
+++ src/components/base/Section.tsx
@@ -11,11 +11,12 @@ const Section = ({ height, children, style }: Props) => {
   const section = css({
     height,
     width: '100%',
-    border: '1px solid #EBEBEB',
+    border: '0.1px solid #EBEBEB',
     borderRadius: '0.8rem',
     boxSizing: 'border-box',
     backgroundColor: '#fff',
     boxShadow: '0 0 0 1px #e3e5e8, 0 1px 2px 0 rgba(0,0,0,.04)',
+    overflow: 'hidden',
     ...style,
   });
 

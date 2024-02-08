fc7d360cc49f9f2b987516ec78347dcca69a596e
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-09 00:30:47 +0900

    fix: 탭 UI 수정

--- src/components/base/Tabs.tsx
+++ src/components/base/Tabs.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
 import { css } from '../../../styled-system/css';
 import classnames from 'classnames';
 
 interface Props {
   categories: string[];
+  currentCategory: number;
+  onChangeCurrentCategory: (num: number) => void;
 }
 
-const Tabs = ({ categories }: Props) => {
-  const [currentCategory, setCurrentCategory] = useState<number>(0);
-
+const Tabs = ({ categories, currentCategory, onChangeCurrentCategory }: Props) => {
   return (
     <div className={tabWrapper}>
       {categories.map((category, index) => {
@@ -21,16 +20,10 @@ const Tabs = ({ categories }: Props) => {
                   tabText,
                   css({ color: currentCategory === index ? '#080808' : '#08080880' }),
                 )}
-                onClick={() => setCurrentCategory(index)}
+                onClick={() => onChangeCurrentCategory(index)}
               >
                 {category}
               </a>
-              {category === '스포츠' && (
-                <div className={live}>
-                  <div className={liveIcon} />
-                  <span className={liveText}>LIVE</span>
-                </div>
-              )}
             </div>
             {categories[0] === '뉴스스탠드' && index === 0 && <div className={dotDivider} />}
             {category !== '뉴스스탠드' && index < categories.length - 1 && (
@@ -75,43 +68,14 @@ const lineDivider = css({
   height: '1.5rem',
   backgroundColor: '#d3d5d7',
   transform: 'skewX(-15deg)',
-  margin: '0 1.1rem',
+  margin: '0 1.2rem',
 });
 
 const dotDivider = css({
   backgroundColor: '#d3d5d7',
-  margin: '0 1.1rem',
+  margin: '0 0.7rem',
   width: '0.4rem',
   height: '0.4rem',
   borderRadius: '0.4rem',
-});
-
-const live = css({
-  width: '4.27rem',
-  height: '1.8rem',
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  backgroundColor: '#f4361e',
-  paddingTop: '0.5rem',
-  paddingBottom: '0.4rem',
-  borderRadius: '0.9rem',
-  marginLeft: '0.5rem',
-});
-
-const liveIcon = css({
-  width: '3rem',
-  height: '2rem',
-  color: '#fff',
-  background: `url(https://pm.pstatic.net/resources/asset/sp_main.2b26eb67.png) -23.6rem -23.5rem`,
-  backgroundPosition: '20px 20px',
-  zoom: '0.4',
-  marginLeft: '0.9rem',
-});
-
-const liveText = css({
-  fontFamily: 'var(--font-nanumsquare-extra)',
-  fontSize: '1.1rem',
-  color: '#fff',
-  textDecoration: 'none',
+  marginBottom: '0.1rem',
 });

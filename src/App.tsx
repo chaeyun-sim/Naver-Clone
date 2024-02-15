33f67dc950e0cb01d6faeda6821ccfbaa40df09a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-15 22:55:10 +0900

    feat: 비밀번호 찾기, 아이디 찾기, 회원가입 및 권한 확인 페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Main from './pages';
+import FindId from './pages/Signup/FindId';
+import FindPassword from './pages/Signup/FindPassword';
+import SignupForm from './pages/Signup/SignupForm';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
         </Route>
+        <Route path="/user2/help/idInquiry" element={<FindId />} />
+        <Route path="/user2/help/pwInquiry" element={<FindPassword />} />
+        <Route path="/user2/join/agree" element={<SignupForm />} />
       </Routes>
     </BrowserRouter>
   );

4cacbe23dfcea725f6e0c08573077a60a34ddced
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-17 12:06:44 +0900

    feat: QueryClient 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,25 @@ import FindId from './pages/Signup/FindId';
 import FindPassword from './pages/Signup/FindPassword';
 import SignupForm from './pages/Signup/SignupForm';
 import CheckPermission from './pages/Signup/CheckPermission';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+let queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-        </Route>
-        <Route path="/user2/help/idInquiry" element={<FindId />} />
-        <Route path="/user2/help/pwInquiry" element={<FindPassword />} />
-        <Route path="/user2/join/agree" element={<CheckPermission />} />
-        <Route path="/user2/join/begin" element={<SignupForm />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Main />} />
+          </Route>
+          <Route path="/user2/help/idInquiry" element={<FindId />} />
+          <Route path="/user2/help/pwInquiry" element={<FindPassword />} />
+          <Route path="/user2/join/agree" element={<CheckPermission />} />
+          <Route path="/user2/join/begin" element={<SignupForm />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 

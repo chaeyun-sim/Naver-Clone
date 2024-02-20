9aebd02ccbe944a1ceb79645dd831b45025ec3bc
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-20 12:47:20 +0900

    feat: 날씨 관련 hook 구현

---
+++ src/hooks/useLargeWeatherIcon.ts
@@ -0,0 +1,34 @@
+const useLargeWeatherIcon = () => {
+  const getWeatherIcon = (weather: string, time: string) => {
+    const hour = Number(time.split(':')[0]);
+    const isNight = hour >= 20 || hour <= 5;
+
+    if (weather.includes('흐림')) {
+      return '-0.1rem -17.4rem';
+    } else if (weather.includes('맑음')) {
+      return isNight ? '-9rem 0' : '0 0';
+    } else if (weather.includes('구름 많음') || weather.includes('튼구름')) {
+      return isNight ? '-17.6rem -8.7rem' : '-17.7rem -0rem';
+    } else if (weather.includes('구름 조금') || weather.includes('약간 구름')) {
+      return isNight ? '-8.9rem -9rem' : '-0.4rem -9rem';
+    } else if (weather.includes('소나기')) {
+      return isNight ? '-52.8rem -43.5rem' : '-17.4rem -25.9rem';
+    } else if (weather.includes('흐리고 한때 비')) {
+      return isNight ? '-0.4rem -43.5rem' : '-9.3rem -34.4rem';
+    } else if (weather.includes('적은 비')) {
+      return '-8.9rem -17rem';
+    } else if (weather.includes('뇌우') || weather.includes('번개')) {
+      return '-34.8rem -8.7rem';
+    } else if (weather.includes('우박')) {
+      return '-34.9rem -17rem';
+    } else if (weather.includes('눈')) {
+      return '-17.7/rem -34.4rem';
+    } else {
+      return '-0.1rem -17.4rem';
+    }
+  };
+
+  return getWeatherIcon;
+};
+
+export default useLargeWeatherIcon;
---
+++ src/hooks/useSmallWeatherIcon.ts
@@ -0,0 +1,28 @@
+const useSmallWeatherIcon = () => {
+  const getIcon = (weather: string, time: string) => {
+    const hour = Number(time.split(':')[0]);
+    const isNight = hour >= 20 || hour <= 5;
+
+    if (weather.includes('맑음') || weather === '화창') {
+      return isNight ? '-3.5rem -52.2rem' : '0 -52.2rem';
+    } else if (weather === '일부 화창') {
+      return isNight ? '-10rem -52.2rem' : '-6.7rem -52.2rem';
+    } else if (weather === '대체로 흐림') {
+      return isNight ? '-16.6rem -52.2rem' : '-13.4rem -52.2rem';
+    } else if (weather === '흐림') {
+      return '-19.8rem -52.2rem';
+    } else if (weather.includes('비')) {
+      return '-23rem -52.2rem';
+    } else if (weather === '많은 비') {
+      return '-26.4rem -52.2rem';
+    } else if (weather === '소나기') {
+      return '-29.7rem -52.2rem';
+    } else {
+      return isNight ? '-16.6rem -52.2rem' : '-13.4rem -52.2rem';
+    }
+  };
+
+  return getIcon;
+};
+
+export default useSmallWeatherIcon;

47df2c259adf5b4f202dbd99510984a0da41ede9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-20 12:46:49 +0900

    feat: 날씨 섹션 UI 구현

--- src/components/Home/RightSection.tsx
+++ src/components/Home/RightSection.tsx
@@ -1,11 +1,22 @@
 import { css } from '../../../styled-system/css';
 import Login from '../LoginSection/Login';
+import Weather from '../RightSection/Weather/Weather';
+import Section from '../base/Section';
 
 const RightSection = () => {
   return (
     <div className={container}>
       <div className={loginWrapper}>
         <Login />
+        <Section style={{ height: '24rem', marginTop: '1.6rem' }}>
+          <a href="https://happybean.naver.com/fundings/detail/F1223">
+            <img
+              src="https://ssl.pstatic.net/melona/libs/1486/1486023/74f29aafb815fef79360_20240202152510385.jpg"
+              className={ad}
+            />
+          </a>
+        </Section>
+        <Weather />
       </div>
     </div>
   );
@@ -20,4 +31,11 @@ const container = css({
 
 const loginWrapper = css({
   height: '16.4rem',
+});
+
+const ad = css({
+  width: '100%',
+  height: '100%',
+  overflow: 'hidden',
+  objectFit: 'fill',
 });

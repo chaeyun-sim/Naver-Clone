a97acf68f3d3b09ebac3ef2c9fadd5b355e2285f
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-20 12:47:33 +0900

    feat: api key 생성

---
+++ src/configs/index.ts
@@ -0,0 +1,2 @@
+export const VITE_APP_OPEN_WEATHER_API_KEY = import.meta.env.VITE_APP_OPEN_WEATHER_API_KEY;
+export const VITE_APP_ACCU_WEATHER_API_KEY = import.meta.env.VITE_APP_ACCU_WEATHER_API_KEY;

6a601b4ff69629017009f7bfd3a5dcd45d7bdcc5
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-02-22 00:46:49 +0900

    feat: 네이버 쇼핑 검색 api를 사용하기 위해 proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/v1': {
+        target: 'https://openapi.naver.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
